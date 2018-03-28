8b8fce171a292c8ee9136c929d4af4a7b5f01c99
Tshevenoc <36426013+Tshevenoc@users.noreply.github.com>
2018-03-28 18:49:46 -0400

    need to figure out reset No real progress made but box colliders are created

---
+++ Assets/mass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mass1 : MonoBehaviour {
+    public float mass;
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = 20f;
+    }
+    
+}
