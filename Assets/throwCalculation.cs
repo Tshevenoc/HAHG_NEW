23420fb0f88658a17d303669992f73d51b04126d
Tshevenoc <36426013+Tshevenoc@users.noreply.github.com>
2018-04-18 20:20:02 -0400

    04/18/2018
    Added Distance and scoreboard. Need to track score and have grenade blowup and end game.

---
+++ Assets/throwCalculation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class throwCalculation : MonoBehaviour {
+
+    private float distance = 0;
+    GameObject horseshoe1;
+    GameObject stake;
+    private bool firstThrow = true;
+    private float currentHighScore = 0;
+    TextMesh highScore;
+    TextMesh currentScore;
+
+    // Use this for initialization
+    void Start () {
+        horseshoe1 = GameObject.FindGameObjectWithTag("Horseshoe");
+        stake = GameObject.FindGameObjectWithTag("Stake");
+        highScore = TextMesh.Find("HighScoretext").GetComponent<TextMesh>();
+        currentScore = TextMesh.Find("CurrentScoretext").GetComponent<TextMesh>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        currentScore.text = distance;
+
+        if (firstThrow == true) {
+            highScore.text = distance;
+            firstThrow = false;
+            currentHighScore = distance;
+        } else if (distance < currentHighScore) {
+            highScore.text = distance;
+            currentHighScore = distance;
+        }else
+        {
+            highScore.text = currentHighScore;
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+
+        Debug.Log(col.gameObject.name);
+        if (col.gameObject.name == "Horseshoe")
+        {
+            distance = Vector3.Distance(horseshoe1.transform.position, stake.transform.position);
+            Debug.Log(distance);
+        }
+    }
+}
